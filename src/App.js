1501aef924e0ed78d471e5d59a659ad8e137ffe3
Philip Brodovsky <philip@clawdia.ai>
2022-11-24 21:47:20 +0200

    Update App.js
    update class name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App2">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
